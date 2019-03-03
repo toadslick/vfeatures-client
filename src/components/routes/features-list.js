ed1dc8cadd1e3e1c06f03b60e50492a9a4ba3a87
James Childress <jchildress@cvent.com>
2019-03-03 05:08:15 -0800

    create form for features

--- src/components/routes/features-list.js
+++ src/components/routes/features-list.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 import connect from '../../utils/refetch/api-connector';
 import RequestResult from '../request-result';
+import RequestForm from '../request-form';
 
 class FeaturesList extends Component {
 
@@ -18,13 +19,22 @@ class FeaturesList extends Component {
   }
 
   render() {
-    const { request } = this.props;
+    const { request, sendRequest } = this.props;
     return (
       <main>
         <h2>Features</h2>
         <RequestResult requests={ [request] }>
           { this.renderResult.bind(this) }
         </RequestResult>
+        <RequestForm
+          url='/features'
+          transform={ state => ({ feature: { ...state }}) }
+          onFulfilled={ sendRequest }
+          values={{ key: '' }}
+          revealButtonContent='Create Feature'
+        >
+          { field => field(<input type='text' name='key'/>) }
+        </RequestForm>
       </main>
     );
   }
@@ -32,4 +42,11 @@ class FeaturesList extends Component {
 
 export default connect(() => ({
   request: `/features`,
+  sendRequest: () => ({
+    request: {
+      url: '/features',
+      refreshing: true,
+      force: true,
+    },
+  }),
 }))(FeaturesList);
