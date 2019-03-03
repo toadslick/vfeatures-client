56e8b0374b710cf2af66605df83fa7c6f3e6d7b2
James Childress <jchildress@cvent.com>
2019-03-03 10:29:37 -0800

    update form for features

--- src/components/routes/feature-detail.js
+++ src/components/routes/feature-detail.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
 import connect from '../../utils/refetch/api-connector';
 import mapBy from '../../utils/map-by-property';
 import RequestResult from '../request-result';
+import RequestForm from '../request-form';
 import DeleteButton from '../delete-button';
 
 class FeatureDetail extends Component {
 
-  renderResult([{ id, key, flags }, releasesMap]) {
+  renderList(flags, releasesMap) {
     const listItems = flags.map(({ id, enabled, release_id }) => {
       const { key } = releasesMap[release_id];
       return (
@@ -20,22 +21,48 @@ class FeatureDetail extends Component {
         </li>
       );
     });
+    return <ul>{ listItems }</ul>;
+  }
+
+  renderResult([{ id, key, flags }, releasesMap]) {
+    const { sendFeatureRequest } = this.props;
+
     return (
-      <main>
+      <Fragment>
         <h2>
           <Link to='/features'>
             Features
           </Link> / { key }
         </h2>
-        <ul>{ listItems }</ul>
+        { this.renderList(flags, releasesMap) }
+        <RequestForm
+          url={ '/features/' + id }
+          method='PUT'
+          transform={ state => ({ feature: { ...state }}) }
+          onFulfilled={ sendFeatureRequest }
+          values={{ key }}
+          revealButtonContent='Edit'
+        >
+          { field => (
+            <label>
+              <span>Key</span>
+              { field(<input
+                type='text'
+                name='key'
+                placeholder='Key'
+                autoComplete='off'
+              />) }
+            </label>
+          )}
+        </RequestForm>
         <DeleteButton
-          requestURL={ `/features/${ id }` }
+          requestURL={ '/features/' + id }
           redirectURL='/features'
           value={ key }
         >
-          Delete { key }
+          Delete
         </DeleteButton>
-      </main>
+      </Fragment>
     );
   }
 
@@ -50,9 +77,16 @@ class FeatureDetail extends Component {
 }
 
 export default connect(({ match: { params: { id }}}) => ({
-  featureRequest: `/features/${ id }`,
+  featureRequest: '/features/' + id,
   releasesRequest: {
     url: `/releases`,
     then: releases => ({ value: mapBy('id', releases) }),
   },
+  sendFeatureRequest: () => ({
+    featureRequest: {
+      url: '/features/' + id,
+      refreshing: true,
+      force: true,
+    },
+  }),
 }))(FeatureDetail);
